dda9715de3543091c27fbeedaf7fe4ad65cca0ef
lee minseop <lms990108@gmail.com>
2024-04-13 16:12:47 +0900

    first commit

---
+++ src/main/java/dankook/cs/aj24/Aj24Application.java
@@ -0,0 +1,13 @@
+package dankook.cs.aj24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aj24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aj24Application.class, args);
+    }
+
+}
---
+++ src/test/java/dankook/cs/aj24/Aj24ApplicationTests.java
@@ -0,0 +1,13 @@
+package dankook.cs.aj24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aj24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
