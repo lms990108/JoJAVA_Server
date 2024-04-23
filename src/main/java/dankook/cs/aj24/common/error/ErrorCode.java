064d4eac56d5aab2aca08cc086a9cb3b62fc4717
lee minseop <lms990108@gmail.com>
2024-04-24 00:23:36 +0900

    Feat: s3설정 및 라우터까지 완성. 다만 테스트 과정에서 이미지가 업로도 되지 않는 오류를 해결중

--- src/main/java/dankook/cs/aj24/common/error/ErrorCode.java
+++ src/main/java/dankook/cs/aj24/common/error/ErrorCode.java
@@ -10,6 +10,9 @@ public enum ErrorCode {
     /* 400 BAD_REQUEST : 잘못된 요청 */
     POST_DELETED(HttpStatus.BAD_REQUEST, "삭제된 게시글입니다."),
     REVIEW_DELETED(HttpStatus.BAD_REQUEST, "삭제된 리뷰입니다."),
+    EMPTY_FILE_EXCEPTION(HttpStatus.BAD_REQUEST, "빈 파일입니다."),
+    NO_FILE_EXTENSION(HttpStatus.BAD_REQUEST, "파일 확장자가 없습니다."),  // 스펠링 수정
+    INVALID_FILE_EXTENSION(HttpStatus.BAD_REQUEST, "허용되지 않는 파일 확장자입니다."),  // 스펠링 수정
 
     /* 401 UNAUTHORIZED : 인증되지 않은 사용자 */
     INVALID_AUTH_TOKEN(HttpStatus.UNAUTHORIZED, "권한 정보가 없는 토큰입니다."),
@@ -20,6 +23,11 @@ public enum ErrorCode {
 
     /* 409 : CONFLICT : Resource의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
     DUPLICATE_RESOURCE(HttpStatus.CONFLICT, "데이터가 이미 존재합니다."),
+
+    /* 500 INTERNAL_SERVER_ERROR : 서버 내부 에러 */
+    IO_EXCEPTION_ON_IMAGE_UPLOAD(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드 중 입출력 오류가 발생했습니다."),
+    PUT_OBJECT_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "S3에 객체를 업로드하는 중 예외가 발생했습니다."),
+    IO_EXCEPTION_ON_IMAGE_DELETE(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 삭제 중 입출력 오류가 발생했습니다.")
     ;
 
     private final HttpStatus httpStatus;
