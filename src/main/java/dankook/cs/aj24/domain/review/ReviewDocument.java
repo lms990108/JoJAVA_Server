c5053342fdeb75e475daf135a97fc0250578a37a
lee minseop <lms990108@gmail.com>
2024-05-18 22:20:45 +0900

    Feat: review도메인 작성자 관련로직 정상동작 확인

--- src/main/java/dankook/cs/aj24/domain/review/ReviewDocument.java
+++ src/main/java/dankook/cs/aj24/domain/review/ReviewDocument.java
@@ -1,5 +1,6 @@
 package dankook.cs.aj24.domain.review;
 
+import dankook.cs.aj24.domain.user.UserDocument;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -24,6 +26,9 @@ public class ReviewDocument {
     private String content;
     private String stars;
     private String imgUrl;
+
+    @DBRef
+    private UserDocument author; // 작성자 정보를 위한 DBRef 필드 추
 
     @CreatedDate
     private LocalDateTime createdAt;
