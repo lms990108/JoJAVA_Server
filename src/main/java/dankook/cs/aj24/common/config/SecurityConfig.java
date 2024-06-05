7c9be799310c885674a24090acedf4730fccc3aa
lee minseop <lms990108@gmail.com>
2024-06-05 14:51:24 +0900

    Hotfix: Cors 설정

--- src/main/java/dankook/cs/aj24/common/config/SecurityConfig.java
+++ src/main/java/dankook/cs/aj24/common/config/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import dankook.cs.aj24.common.handler.CustomSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration  // 스프링의 설정 클래스를 정의하는 어노테이션
 @EnableWebSecurity  // 스프링 시큐리티의 웹 보안 기능을 활성화
@@ -34,7 +39,7 @@ public class SecurityConfig {
     @Bean  // 스프링 컨테이너에 의해 관리되는 빈 객체를 생성
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)  // CSRF(Cross-Site Request Forgery) 보호 기능을 비활성화
                 .formLogin(AbstractHttpConfigurer::disable)  // 스프링 시큐리티의 기본 로그인 페이지를 비활성화
                 .httpBasic(AbstractHttpConfigurer::disable)  // HTTP 기본 인증 비활성화
@@ -59,5 +64,19 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // 세션을 생성하지 않고, 상태를 유지하지 않는 정책 설정
 
                 .build();  // HttpSecurity 객체를 사용하여 SecurityFilterChain을 생성
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
