b403429efba09ce34720760a51d8f73d6778064c
lee minseop <lms990108@gmail.com>
2024-06-07 17:23:53 +0900

    Chore: https://accounts.kakao.com 주소도 cors 설정에 추가

--- src/main/java/dankook/cs/aj24/common/config/SecurityConfig.java
+++ src/main/java/dankook/cs/aj24/common/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://220.149.232.224:8080"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://220.149.232.224:8080", "https://accounts.kakao.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
